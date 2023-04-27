0b67d7b796655273d8aefcb97955241cce6dd45a
Smo7e <120028063+Smo7e@users.noreply.github.com>
2023-04-27 14:07:32 +0400

    main

--- src/components/calculator/useCalculator.js
+++ src/components/calculator/useCalculator.js
@@ -1,22 +1,14 @@
-import { Calculator, PolynomialCalculator } from '../calculator';
+import { Calculator } from "../calculator";
 
-export default function useCalculator(refA, refB, refC, refPolyA, refPolyB, refPolyC) {
-  const calc = new Calculator();
-  const calcPoly = new PolynomialCalculator();
+export default function useCalculator(refA, refB, refC) {
+    const calc = new Calculator();
 
-  console.log(refA);
-
-  return (operand) => {
-    if (refA && refB && refC) {
-      const A = refA.current.value;
-      const B = refB.current.value;
-      refC.current.value = calc[operand](calc.getEntity(A), calc.getEntity(B));
-      refC.toString();
-    }
-    if (refPolyA && refPolyB && refPolyC) {
-      const A = refPolyA.current.value;
-      const B = refPolyB.current.value;
-      refPolyC.current.value = calcPoly[operand](calcPoly.getPolynomial(A), calcPoly.getPolynomial(B));
-    }
-  };
+    return (operand) => {
+        if (refA && refB && refC) {
+            const A = refA.current.value;
+            const B = refB.current.value;
+            refC.current.value = calc[operand](calc.getEntity(A), calc.getEntity(B));
+            refC.toString();
+        }
+    };
 }
